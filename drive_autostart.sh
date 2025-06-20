1d0ac52c9435d1b5a679a28c81a95e6ac524c92c
Thomas Béraud <60184236+Thomas-Beraud@users.noreply.github.com>
2025-06-19 21:37:46 -0400

    Create drive_autostart.sh

---
+++ drive_autostart.sh
@@ -0,0 +1,8 @@
+echo "Create Remote Drive AutoStart"
+cd ~
+mkdir 1_EQUIPE 2_PROJETS OneDrive .config/autostart
+mv /Downloads/onedrive.desktop .config/autostart/
+mv /Downloads/1_EQUIPE.desktop .config/autostart/
+mv /Downloads/2_PROJETS.desktop .config/autostart/
+echo "Finished!! "
+pause
